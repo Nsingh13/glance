65ce34334903aa1a1d1d2c86d14743910a6ef2bf
Navjot Singh <13.navjot.singh@gmail.com>
2017-06-20 14:52:24 -0700

    Search Screen Full Design Completed

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import {
+  Container,
+  Header,
+  Item,
+  Input,
+  Icon,
+  Button,
+  Content,
+  Text
+} from 'native-base';
+
+import PlacesTile from '../components/searchScreen/PlacesTile';
+
 export default class SearchScreen extends React.Component {
 
   render() {
     return (
-      <View>
-
-      </View>
+      <Container>
+      <PlacesTile />
+      <Button
+            dark
+            block
+            iconLeft
+             style={{marginTop: '3%'}}
+           >
+            <Icon name='md-call'/>
+            <Text>Find By Phone</Text>
+          </Button>
+          <Button
+            dark
+            block
+            iconLeft
+            style={{marginTop: '4%', marginBottom: '9%'}}
+           >
+            <Icon name='md-mail'/>
+            <Text>Find By Email</Text>
+          </Button>
+      </Container>
     );
   }
 }
